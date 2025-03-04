9f991226a03fa4ce8af0d962148c2dfcb85860ca
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-03-04 09:57:42 +0100

    add the script for generating the server skeleton

---
+++ generate_server_skeleton.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#tmpdir=$(mktemp -d /var/tmp/XXXXXX)
+#for s in data-center-facility/facility-api.spec.yaml rack/rack-api.spec.yaml server/server-api.spec.yaml ; do
+#	curl -O --output-dir $tmpdir https://raw.githubusercontent.com/SDIAlliance/nadiki-api/refs/heads/main/$s
+#done
+
+specdir=~/work/leitmotiv/nadiki-api
+docker run -v $specdir:/local -w /local redocly/cli join data-center-facility/facility-api.spec.yaml rack/rack-api.spec.yaml server/server-api.spec.yaml -o nadiki-api.yaml
+
+docker run --user $(id -u) --rm -v "${PWD}:/local/out" -v "${specdir}:/local/in" openapitools/openapi-generator-cli:v6.6.0 generate \
+    -i /local/in/nadiki-api.yaml \
+    -g python-flask \
+    -o /local/out \
+    --package-name nadiki_registrar
+#    --skip-validate-spec \
+
+#rm -r $tmpdir
