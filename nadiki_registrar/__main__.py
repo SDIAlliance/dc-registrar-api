8229956465101543e72e5169b5d9c9a75782766f
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-03-31 09:22:04 +0200

    implement gunicorn and a production Docker build

--- nadiki_registrar/__main__.py
+++ nadiki_registrar/__main__.py
@@ -12,8 +12,9 @@ def main():
                 arguments={'title': 'Facility Registry API'},
                 pythonic_params=True)
 
-    app.run(port=8080)
+    #app.run(port=8080)
+    return app
 
 
 if __name__ == '__main__':
-    main()
+    main().run(port=8080)
